ffd525a7b85f1d5bea800e234d98ce13cdfe6ea3
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-08-30 17:46:45 -0300

    first draft

--- src/SaiProxy.sol
+++ src/SaiProxy.sol
@@ -1,4 +1,96 @@
 pragma solidity ^0.4.16;
 
-contract SaiProxy {
+import "ds-thing/thing.sol";
+
+contract TubInterface {
+    function open() returns (bytes32);
+    function join(uint128);
+    function lock(bytes32, uint128);
+    function draw(bytes32, uint128);
+    function cups(bytes32) returns (address, uint128, uint128);
+    function gem() returns (address);
+    function skr() returns (address);
+    function sai() returns (address);
+    function jar() returns (address);
+    function tip() returns (address);
+    function mat() returns (uint128);
+    function tab(bytes32) returns (uint128);
+    function chi() returns (uint128);
+}
+
+contract TokenInterface {
+    function approve(address, uint);
+    function transfer(address, uint) returns (bool);
+    function transferFrom(address, address, uint) returns (bool);
+}
+
+contract JarInterface {
+    function per() returns (uint128);
+    function tag() returns (uint128);
+}
+
+contract TipInterface {
+    function par() returns (uint128);
+}
+
+contract SaiProxy is DSThing {
+    TubInterface public tub;
+    TokenInterface gem;
+    TokenInterface skr;
+    TokenInterface sai;
+    JarInterface   jar;
+    TipInterface   tip;
+
+    function SaiProxy(address _tub) {
+        tub = TubInterface(_tub);
+        gem = TokenInterface(tub.gem());
+        skr = TokenInterface(tub.skr());
+        sai = TokenInterface(tub.sai());
+        jar = JarInterface(tub.jar());
+        tip = TipInterface(tub.tip());
+    }
+
+    function pull(address tok, uint128 wad) auth {
+        assert(TokenInterface(tok).transfer(owner, wad));
+    }
+
+    // function push(address tok, uint128 wad) auth {
+    //     assert(TokenInterface(tok).transferFrom(owner, this, wad));
+    // }
+
+    /**
+    * Draws 'wad' amount of SAI locking enough SKR to keep the CDP with 'mat' percentage of collateralization
+    *
+    * @param    cup    CDP ID
+    * @param    wad    Amount of SAI to draw
+    * @param    mat    collateralization of CDP after drawing
+    */
+    function draw(bytes32 cup, uint128 wad, uint128 mat) auth {
+        require(mat >= tub.mat());
+        var (,cart,cink) = tub.cups(cup);
+        uint128 ink = hsub(wdiv(rmul(wmul(tip.par(), rmul(hadd(wad, cart), tub.chi())), mat), jar.tag()), cink);
+        uint128 jam = rmul(ink, jar.per());
+        gem.approve(jar, jam);
+        tub.join(jam);
+        skr.approve(jar, ink);
+        tub.lock(cup, ink);
+        tub.draw(cup, wad);
+    }
+
+    function draw(uint128 wad, uint128 ratio) auth {
+        var cup = tub.open();
+        draw(cup, wad, ratio);
+    }
+
+    /**
+    * Wipes 'wad' amount of SAI leaving enough locked SKR to keep the CDP with 'mat' percentage of collateralization
+    *
+    * @param    cup    CDP ID
+    * @param    wad    Amount of SAI to wipe
+    * @param    mat    collateralization of CDP after wiping
+    */
+    // function wipe(bytes32 cup, uint128 wad, uint128 mat) auth {
+    //     require(mat >= tub.mat());
+    //     var (,cart,cink) = tub.cups(cup);
+    // }
 }
--- src/SaiProxy.t.sol
+++ src/SaiProxy.t.sol
@@ -1,21 +1,31 @@
 pragma solidity ^0.4.16;
 
 import "ds-test/test.sol";
+import "sai/sai.t.sol";
 
 import "./SaiProxy.sol";
 
-contract SaiProxyTest is DSTest {
+contract SaiProxyTest is SaiTestBase {
     SaiProxy proxy;
 
     function setUp() {
-        proxy = new SaiProxy();
+        super.setUp();
+        setPublicRoles();
+        proxy = new SaiProxy(tub);
+        tub.cork(1000 ether);
     }
 
-    function testFail_basic_sanity() {
-        assert(false);
+    function testProxyDraw() {
+        assertEq(sai.balanceOf(this), 0 ether);
+        gem.transfer(proxy, 100 ether);
+        proxy.draw(50 ether, ray(1.5 ether));
+        proxy.pull(sai, 50 ether);
+
+        assertEq(sai.balanceOf(this), 50 ether);
     }
 
-    function test_basic_sanity() {
-        assert(true);
+    function testFailProxyDrawBelowTubMat() {
+        gem.transfer(proxy, 100 ether);
+        proxy.draw(50 ether, ray(0.9999 ether));
     }
 }
