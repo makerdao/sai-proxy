3e712703b9c2414d33bba7b74bf50d447abf45b8
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-06-27 15:06:54 -0300

    Use proxy factory interface

--- src/ProxyCreationAndExecute.sol
+++ src/ProxyCreationAndExecute.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.4.23;
 
 import "./SaiProxy.sol";
-import "ds-proxy/proxy.sol";
+
+contract ProxyFactoryInterface {
+    function build(address) public returns (address);
+}
 
 contract ProxyCreationAndExecute is SaiProxy {
     function createLockAndDraw(address factory_, address tub_, uint wad) public payable returns (address proxy, bytes32 cup) {
-        proxy = DSProxyFactory(factory_).build(msg.sender);
+        proxy = ProxyFactoryInterface(factory_).build(msg.sender);
         cup = open(tub_);
         lockAndDraw(tub_, cup, wad);
         TubInterface(tub_).give(cup, proxy);
