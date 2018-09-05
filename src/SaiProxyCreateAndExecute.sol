c4e16c10be0a66079db21eebcdb04cf7b5bf077a
Michael Elliot <michael@digital.com.au>
2018-09-05 10:38:30 +0800

    Rename contract interface and argument name
    To improve clarity:
    - Rename ProxyFactoryInterface to ProxyRegistryInterface
    - Rename createLockAndDraw() method parameter factory_ to registry_

--- src/SaiProxyCreateAndExecute.sol
+++ src/SaiProxyCreateAndExecute.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.23;
 
 import "./SaiProxy.sol";
 
-contract ProxyFactoryInterface {
+contract ProxyRegistryInterface {
     function build(address) public returns (address);
 }
 
 contract SaiProxyCreateAndExecute is SaiProxy {
-    function createLockAndDraw(address factory_, address tub_, uint wad) public payable returns (address proxy, bytes32 cup) {
-        proxy = ProxyFactoryInterface(factory_).build(msg.sender);
+    function createLockAndDraw(address registry_, address tub_, uint wad) public payable returns (address proxy, bytes32 cup) {
+        proxy = ProxyRegistryInterface(registry_).build(msg.sender);
         cup = open(tub_);
         lockAndDraw(tub_, cup, wad);
         TubInterface(tub_).give(cup, proxy);
