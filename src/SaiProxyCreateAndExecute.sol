e1581fdace4e4d02e8880e122a43092e5a805bbe
Michael Elliot <michael@digital.com.au>
2018-09-10 12:02:26 +0800

    Add createAndOpen() and createOpenAndLock() methods
    - createAndOpen() creates a DSProxy instance and opens a cup
    - createOpenAndLock() creates a DSProxy instance, opens a cup, and locks collateral

--- src/SaiProxyCreateAndExecute.sol
+++ src/SaiProxyCreateAndExecute.sol
@@ -7,6 +7,23 @@ contract ProxyRegistryInterface {
 }
 
 contract SaiProxyCreateAndExecute is SaiProxy {
+
+    // Create a DSProxy instance and open a cup
+    function createAndOpen(address registry_, address tub_) public returns (address proxy, bytes32 cup) {
+        proxy = ProxyRegistryInterface(registry_).build(msg.sender);
+        cup = open(tub_);
+        TubInterface(tub_).give(cup, proxy);
+    }
+
+    // Create a DSProxy instance, open a cup, and lock collateral
+    function createOpenAndLock(address registry_, address tub_) public payable returns (address proxy, bytes32 cup) {
+        proxy = ProxyRegistryInterface(registry_).build(msg.sender);
+        cup = open(tub_);
+        lock(tub_, cup);
+        TubInterface(tub_).give(cup, proxy);
+    }
+
+    // Create a DSProxy instance, open a cup, lock collateral, and draw DAI
     function createOpenLockAndDraw(address registry_, address tub_, uint wad) public payable returns (address proxy, bytes32 cup) {
         proxy = ProxyRegistryInterface(registry_).build(msg.sender);
         cup = open(tub_);
